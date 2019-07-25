90323a501ff415fb5f74837a4dadaf2b7f992d32
Juan Montoya <Juan.Montoya@orbitgroup.ca>
2019-07-25 16:09:26 -0500

    Fixes and generic implementation of form in users

--- content/4.UI/Company.Project.UI/ClientApp/src/app/security/users/user/user.component.ts
+++ content/4.UI/Company.Project.UI/ClientApp/src/app/security/users/user/user.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { UserService } from '../services/user.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
@@ -7,7 +7,8 @@ import { Base64 } from 'src/app/shared/utils/base64';
 import { finalize, take } from 'rxjs/operators';
 import { LoadingService } from 'src/app/shared/services/loading/loading.service';
 import { Users } from '../users.models';
-import { FormConfig, FormResponse } from 'src/app/shared/components/form/form.models';
+import { FormConfig } from 'src/app/shared/components/form/form.models';
+import { FormComponent } from 'src/app/shared/components/form/form.component';
 
 @Component({
   selector: 'app-user',
@@ -15,18 +16,10 @@ import { FormConfig, FormResponse } from 'src/app/shared/components/form/form.mo
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit, OnDestroy {
+  @ViewChild('form') appForm: FormComponent;
   editMode: boolean;
   defaultPass = '**********';
-  // userForm = this.fb.group({
-  //   username: ['', Validators.compose([
-  //     Validators.required, Validators.minLength(3), Validators.maxLength(15)])],
-  //   email: ['', Validators.compose([
-  //     Validators.required, Validators.minLength(3), Validators.maxLength(50)])],
-  //   roleId: [null, Validators.compose([
-  //     Validators.required])],
-  //   password: ['', Validators.compose([
-  //     Validators.required, Validators.minLength(8), Validators.maxLength(50)])]
-  // });
+  id: number;
 
   config: FormConfig = {
     appearance: 'standard',
@@ -79,7 +72,6 @@ export class UserComponent implements OnInit, OnDestroy {
   };
 
   constructor(
-    private fb: FormBuilder,
     private router: Router,
     private route: ActivatedRoute,
     private snackBar: MatSnackBar,
@@ -91,6 +83,7 @@ export class UserComponent implements OnInit, OnDestroy {
     this.route.params.pipe(take(1)).subscribe((data) => {
       this.editMode = typeof (data.id) !== 'undefined';
       if (this.editMode) {
+        this.id = data.id;
         this.get(data.id);
       }
     });
@@ -99,44 +92,21 @@ export class UserComponent implements OnInit, OnDestroy {
   get(id: number) {
     this.userService.get(id).pipe(take(1)).subscribe((user) => {
       user.password = this.defaultPass;
-      // this.userForm.setValue({
-      //   username: user.username,
-      //   email: user.email,
-      //   roleId: user.roleId,
-      //   password: user.password
-      // });
+      this.appForm.form.setValue({
+        username: user.username,
+        email: user.email,
+        roleId: user.roleId,
+        password: user.password
+      });
     });
   }
 
-  // onSubmit() {
-  //   if (this.userForm.valid) {
-  //     this.loading.show();
-  //     const finalizeFunction = finalize(() => this.loading.hide());
-  //     const user = this.userForm.value as Users;
-  //     user.password = Base64.encode(user.password);
-  //     if (this.editMode) {
-  //       if (user.password === Base64.encode(this.defaultPass)) {
-  //         user.password = '';
-  //       }
-  //       this.userService.update(user).pipe(take(1), finalizeFunction).subscribe(() => {
-  //         this.snackBar.open(`User "${user.username}" has been updated.`, 'Dismiss', { duration: 3000 });
-  //         this.goBack();
-  //       });
-  //     } else {
-  //       this.userService.create(user).pipe(take(1), finalizeFunction).subscribe(() => {
-  //         this.snackBar.open(`User "${user.username}" has been created.`, 'Dismiss', { duration: 3000 });
-  //         this.goBack();
-  //       });
-  //     }
-  //   }
-  // }
-
   onSubmit(user: Users) {
-    console.log(user)
     this.loading.show();
     const finalizeFunction = finalize(() => this.loading.hide());
     user.password = Base64.encode(user.password);
     if (this.editMode) {
+      user.id = this.id;
       if (user.password === Base64.encode(this.defaultPass)) {
         user.password = '';
       }
